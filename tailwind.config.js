41d56f5efc12b7e6cc32350e853e26e768d6eadf
KannadukiAme <misuzunagisa@gmail.com>
2023-02-09 19:40:26 +0800

    update icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,44 +19,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    plugin(function ({ addComponents }) {
-      const tags = {
-        '.tag': {
-          backgroundColor: '#fff',
-          borderWidth: '1px',
-          borderColor: '#1a202c',
-          padding: '0.25rem 1rem 0.25rem 1rem',
-          color: '#1a202c',
-          '&:hover': {
-            backgroundColor: '#1a202c',
-            borderColor: '#fff',
-            color: '#fff',
-            cursor: 'pointer',
-          },
-        },
-        '.tag--active': {
-          borderWidth: '1px',
-          padding: '0.25rem 1rem 0.25rem 1rem',
-          backgroundColor: '#1a202c',
-          borderColor: '#fff',
-          color: '#fff',
-        },
-        '.tag--white': {
-          borderWidth: '1px',
-          borderColor: 'white',
-          padding: '0.25rem 1rem 0.25rem 1rem',
-          color: 'white',
-          '&:hover': {
-            borderColor: '#58B2DC',
-            color: '#58B2DC',
-            cursor: 'pointer',
-          },
-        },
-      }
-
-      addComponents(tags)
-    }),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 }
